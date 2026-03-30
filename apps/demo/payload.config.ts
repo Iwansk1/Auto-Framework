0c16f16ae8504dc7785667e687bc6a61a7399915
Iwan Bijl <Iwanbijl@gmail.com>
2026-03-30 16:38:57 +0200

    Might work with media now

--- apps/demo/payload.config.ts
+++ apps/demo/payload.config.ts
@@ -10,6 +10,7 @@ import { Colors } from "./collections/Colors";
 import { Wheels } from "./collections/Wheels";
 import { Packages } from "./collections/Packages";
 import { Occasions } from "./collections/Occasions";
+import { s3Storage } from "@payloadcms/storage-s3";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
@@ -21,16 +22,40 @@ export default buildConfig({
             baseDir: path.resolve(dirname),
         },
     },
+
     collections: [Users, Media, Vehicles, Colors, Wheels, Packages, Occasions],
+
     editor: lexicalEditor(),
+
     secret: process.env.PAYLOAD_SECRET || "",
+
     typescript: {
         outputFile: path.resolve(dirname, "payload-types.ts"),
     },
+
     db: postgresAdapter({
         pool: {
             connectionString: process.env.DATABASE_URI || "",
         },
     }),
-    plugins: [],
+
+    plugins: [
+        s3Storage({
+            collections: {
+                media: true,
+                // optionally:
+                // 'media-with-prefix': { prefix: 'some-prefix/' },
+            },
+            bucket: process.env.SUPABASE_BUCKET!,
+            config: {
+                endpoint: process.env.SUPABASE_ENDPOINT,
+                credentials: {
+                    accessKeyId: process.env.SUPABASE_ACCESS_KEY!,
+                    secretAccessKey: process.env.SUPABASE_SECRET_KEY!,
+                },
+                region: process.env.SUPABASE_REGION,
+                forcePathStyle: true,
+            },
+        }),
+    ],
 });
